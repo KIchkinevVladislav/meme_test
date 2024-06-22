b06e9f344b055060b9872660334aa67ebc5d5787
Vladislav <v.kichkinev@microline.ru>
2024-06-22 20:13:40 +0300

    fix: add config for db

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-SECRET_KEY = 'secret_key'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 240
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'postgres'
+DB_USER = 'postgres'
+DB_PASS = 'postgres'
 
 MINIO_ENDPOINT = 'localhost:9000'
 MINIO_ACCESS_KEY = 'minioadmin'
 MINIO_SECRET_KEY = 'minioadmin'
+
+SECRET_KEY = 'secret_key'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 240
--- database/db.py
+++ database/db.py
@@ -2,13 +2,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 from sqlalchemy.orm import declarative_base
 
 from typing import AsyncGenerator
 
+from config import (DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS,)
 
-REAL_DATABASE_URL ="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
+REAL_DATABASE_URL =f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(
     REAL_DATABASE_URL,
